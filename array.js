e13a71fe232f0ee61c0bfb9aa636022f87dcf640
PaulOlalude <Paulolaludee@gmail.com>
2022-04-08 21:54:39 +0100

    Initial commit

---
+++ array.js
@@ -0,0 +1,21 @@
+var fruits = ["banana", "Apples", "Oranges", "Blueberries"]
+
+// 1.. remove the banana from the array.
+fruits.shift();
+
+//2.. sort the array in order.
+fruits.sort();
+
+//3..put "kiwi" at the end of the array.
+fruits.push();
+
+//4.. remove "apples" from the array
+fruits.shift();
+
+//5.. sort the array in reverse order
+fruits.reverse();
+
+// using this array, 
+var array2 = ["Banana", ["Apples",["oranges"], "Blueberries"]];
+
+console.log(array2[1][1][0]);
---
+++ object.js
@@ -0,0 +1,44 @@
+// create an object and an array which we will use in our fb exercise.
+//1. create an object that has properties "username" and "password". fill those with strings.
+//2. Create an array which contains the object you made above and name the array database.
+//3. create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
+
+var user = {
+    username: "Paulolondon"
+    password: "CR7Messi10"
+
+};
+
+console.log(user);
+
+var userdetails= [
+
+    {
+        username: "paulolondon"
+        password: "CR7Messi10"
+    },
+
+    {
+        username: "Diddy"
+        password: "javascript"
+    }
+];
+console.log(userdetails);
+
+var newsfeed = [
+    { username: "Timitomi"
+      timeline: "1234"
+    },
+
+    { username: "Akorede-Daniel"
+      timeline: " dadadada"
+      
+    },
+
+    { username: "Buhari"
+      timeline: "omox100"
+
+    }
+];
+ console.info(newsfeed);
+
