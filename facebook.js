fa8b531216390cb05a6058cdf2ba27affdd3997f
PaulOlalude <Paulolaludee@gmail.com>
2022-04-14 21:24:01 +0100

    uploading

---
+++ facebook.js
@@ -0,0 +1,68 @@
+// create an object with properties "username", "password" and "status".
+
+var facebookID={ username: "PaBubu",
+password: "failedpromises",
+status: "howmuchisdollars"
+}
+
+//create an array named database for Q1
+
+var database = [
+    { username: "PaBubu",
+    password: "1234",
+    status: "howmuchisdollars"
+    },
+    { username: "VPOsibajo"
+    password: "trabaye",
+    status: "condolencevisit"
+    }
+];
+
+// Accept username, password, & status from the prompt and add to the database array.
+var username = prompt("what is your username"),
+ var password= prompt("what is your password?"),
+ var status=  prompt("how do you feel today?"),
+var database = [
+    { username: "PaBubu",
+    password: "1234",
+    status: "howmuchisdollars"
+    },
+
+    { username: "VPOsibajo"
+    password: "trabaye",
+    status: "condolencevisit"
+    }
+];
+
+// create a funtion called signin that accept two paraeters- username, password. This function would the database array to check for any 
+// matching entry. if username and password matches, return both username and status, if not, return "wrong username and password".
+var database = [
+    { username: "PaBubu",
+    password: "1234",
+    status: "howmuchisdollars"
+    },
+
+    { username: "VPOsibajo"
+    password: "trabaye",
+    status: "condolencevisit"
+    }
+]
+function signin(username, password){ if (var username === "PaBubu" && var password==="1234" || var username === "VPOsibajo" && password ==="trabaye"){
+    return username, password;
+
+else return ' sorry, username not correct'
+
+
+}
+
+}
+
+
+ 
+
+
+
+
+
+
+
